18ef1fdc73f91486dac8906dff493a88eb806259
Mary <umohm12@gmail.com>
2023-12-18 12:44:59 -0500

    wip

--- P1/entertainment-app/src/App.jsx
+++ P1/entertainment-app/src/App.jsx
@@ -1,25 +1,45 @@
 import React from 'react';
-import { Route, createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { Route, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import SideNav from '../src/components/SideNav';
+import TvSeries from './components/TvSeries';
+import Home from './components/Home';
+
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 
 // https://medium.com/@galohernandez/vite-react-react-router-dom-the-latest-way-312ee887197e
 
-const router = createBrowserRouter(
-   createRoutesFromElements(
-     <Route>
-      <Route index element={<SideNav />} />
-     </Route>
-   )
- )
+// const router = createBrowserRouter(
+//    createRoutesFromElements(
+//      <Route>
+//       <Route index element={<SideNav />} />
+//      </Route>
+//    )
+//  )
 
-function App({ routes }) {
+// function App({ routes }) {
 
+//   return (
+//     <>
+//       <RouterProvider router={router} />
+//     </>
+//   );
+// }
+
+function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+    <BrowserRouter>
+      <Routes>
+        {/* <SideNav /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/tvseries" element={<TvSeries />} />
+      </Routes>
+    </BrowserRouter>
+  ); 
 }
 
 export default App;
--- P1/entertainment-app/src/components/SideNav.jsx
+++ P1/entertainment-app/src/components/SideNav.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Home from './Home';
 import TvSeries from './TvSeries';
 
 
 function SideNav() {
   return (
-    <div className='text-yellow'>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">{Home}</Link>
-          </li>
-          <li>
-            <Link to="/TvSeries">{TvSeries}</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <h1>SideNav</h1>
+    // <div className='text-yellow'>
+    //   <nav>
+    //     <ul>
+    //       <li>
+    //         <Link to="/">{Home}</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/TvSeries">{TvSeries}</Link>
+    //       </li>
+    //     </ul>
+    //   </nav>
+    // </div>
   )
 }
 
