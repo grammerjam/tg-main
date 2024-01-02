16f78b8c49487e43f3ffc228c63d835c25d55847
alv-314 <alicov314@gmail.com>
2024-01-01 22:02:29 -0500

    skeleton for react form

---
+++ P1/entertainment-app/src/pages/SignUp.jsx
@@ -0,0 +1,25 @@
+
+const SignUp = () => {
+    return (
+        <>
+        {/* Icon */}
+            <div>
+
+            </div>
+        {/* Form Card*/}
+            <div className="flex justify-center">
+                <h2> Sign Up </h2>
+                <form>
+                    <label> Email Address</label>
+                    <input> </input>
+                    <label> Password </label>
+                    <input> </input>
+                    <label> Repeat Password </label>
+                    <input> </input>
+                </form>
+            </div>   
+        </>
+    )
+}
+
+export default SignUp;
