0a48bd5486037990c34eb16ee809cf469da00c71
Jose A <jfacademusic@gmail.com>
2024-01-01 21:56:23 -0500

    added error message

--- P1/entertainment-app/src/components/LoginModal.jsx
+++ P1/entertainment-app/src/components/LoginModal.jsx
@@ -5,11 +5,13 @@ export default function LoginModal() {
     const { isLoaded, signIn, setActive } = useSignIn();
     const [emailAddress, setEmailAddress] = useState("");
     const [password, setPassword] = useState("");
+    const [hasSubmited, setHasSubmited] = useState(false)
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (!isLoaded) {
             return;
         }
+        setHasSubmited(true)
 
         try {
             const result = await signIn.create({
@@ -32,11 +34,20 @@ export default function LoginModal() {
     };
     return (
         <div className="rounded-[10px] bg-ma-blue p-[24px] pb-[32px] w-[100%] tablet:max-w-[400px] tablet:p-[32px]">
-            <h1 className="text-h-sm font-light pb-[40px]">Login</h1>
+            <h1 className="text-h-lg font-light pb-[40px]">Login</h1>
             <form className="flex flex-col gap-[40px] pb-[24px]">
                 <div className="flex flex-col gap-[24px]">
-                    <input className="bg-transparent border-b-[1px] border-ma-gray pb-[18px] pl-[16px] placeholder:font-light" placeholder="Email address" onChange={(e) => setEmailAddress(e.target.value)} id="email" name="email" type="email" />
-                    <input className="bg-transparent border-b-[1px] border-ma-gray pb-[18px] pl-[16px] placeholder:font-light" placeholder="Password" onChange={(e) => setPassword(e.target.value)} id="password" name="password" type="password" />
+                    <div className="w-[100%] relative h-[44px]">
+                        <input className={`w-[100%] bg-transparent border-b-[1px] border-ma-gray pb-[18px] pl-[16px] text-b-med placeholder:font-light font-light focus:outline-none focus:border-ma-white caret-ma-red ${hasSubmited && emailAddress === "" ? "border-b-ma-red" : ""}`} placeholder="Email address"
+                            onChange={(e) => setEmailAddress(e.target.value)}
+                            id="email"
+                            name="email"
+                            type="email" />
+                        
+                            <p className="absolute text-ma-red text-b-sm right-0 top-0">Can&apos;t be empty</p>
+                            
+                    </div>
+                    <input className={`bg-transparent border-b-[1px] border-ma-gray pb-[18px] pl-[16px] text-b-med placeholder:font-light font-light focus:outline-none focus:border-ma-white caret-ma-red ${hasSubmited && password === "" ? "border-b-ma-red" : ""}`} placeholder="Password" onChange={(e) => setPassword(e.target.value)} id="password" name="password" type="password" />
                 </div>
                 <button className="font-light bg-ma-red rounded-[6px] py-[14px]" onClick={handleSubmit}>Login to your account</button>
             </form>
