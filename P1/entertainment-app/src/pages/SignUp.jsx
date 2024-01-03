410b37d41c3a90a334ae929ebdd45c56a1c5547d
alv-314 <alicov314@gmail.com>
2024-01-03 10:32:14 -0500

    Finished the UI skeleton

--- P1/entertainment-app/src/pages/SignUp.jsx
+++ P1/entertainment-app/src/pages/SignUp.jsx
@@ -1,24 +1,30 @@
+import Button from "../components/Button";
 
 const SignUp = () => {
     return (
-        <>
+        <main className="flex h-screen items-center justify-center">
         {/* Icon */}
             <div>
 
             </div>
         {/* Form Card*/}
-            <div className="flex justify-center">
+            <div className="h-[27rem] w-[20rem] bg-ma-blue flex flex-col justify-center">
                 <h2> Sign Up </h2>
-                <form>
+                <form className="flex flex-col">
                     <label> Email Address</label>
                     <input/>
                     <label> Password </label>
                     <input/>
                     <label> Repeat Password </label>
                     <input/>
+                    <Button text={"Submit"}></Button>
+                    <span> 
+                        <p> Already have an account? </p>
+                        <a> Login </a>
+                    </span>
                 </form>
             </div>   
-        </>
+        </main>
     )
 }
 
