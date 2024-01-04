236998ccf78babf33e4132a848f8c389aa06b564
Jose A <jfacademusic@gmail.com>
2024-01-04 02:38:47 -0500

    minor spacing changes

--- P1/entertainment-app/src/pages/Login.jsx
+++ P1/entertainment-app/src/pages/Login.jsx
@@ -4,7 +4,7 @@ import LoginModal from "../components/LoginModal";
 const Login = () => {
 
     return (
-        <div className="w-[100%] h-[100%] px-[24px] pt-[48px] gap-[58px] flex flex-col items-center">
+        <div className="w-[100dvw] h-[100dvh] px-[24px] pt-[48px]  gap-[58px] flex flex-col items-center tablet:pt-[80px] tablet:gap-[72.4px] desktop:gap-[83px]">
             <img src={Logo}/>
             <LoginModal />
         </div>
