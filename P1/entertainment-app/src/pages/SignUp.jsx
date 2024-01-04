aaecf7994c74d763860edce6ca0b9fdd0d7efe77
alv-314 <alicov314@gmail.com>
2024-01-04 02:42:50 -0500

    Added react state to the signup form

--- P1/entertainment-app/src/pages/SignUp.jsx
+++ P1/entertainment-app/src/pages/SignUp.jsx
@@ -1,29 +1,42 @@
 import Button from "../components/Button";
+import { useState } from "react";
 
 const SignUp = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [repeatPassword, setRepeatPassword] = useState("");
+
+
     return (
         <main className="flex h-screen items-center justify-center">
-        {/* Icon */}
+            {/* Icon */}
             <div>
 
             </div>
-        {/* Form Card*/}
+            {/* Form Card*/}
             <div className="min-h-[27rem] w-[20rem] p-[1.5rem] rounded-[0.66rem] bg-ma-blue flex flex-col">
                 <h2 className=" text-[2.5rem] mb-[2.5rem]"> Sign Up </h2>
                 <form className="flex flex-col">
-                    <label className=" text-ma-gray"> Email Address</label>
-                    <input className=" mb-[1.5rem]"/>
-                    <label className=" text-ma-gray"> Password </label>
-                    <input className=" mb-[1.5rem]"/>
-                    <label className=" text-ma-gray"> Repeat Password </label>
-                    <input className=" mb-[2.5rem]"/>
+                    <label className=" text-ma-gray mb-[0.5rem]"> Email Address</label>
+                    <input onChange={(e) => {
+                        setEmail(e.target.value)
+                    }} value={email} className=" mb-[1.5rem] bg-transparent border-b-[1px] border-b-ma-gray" />
+                    <label className=" text-ma-gray mb-[0.5rem]"> Password </label>
+                    <input onChange={(e) => {
+                        setPassword(e.target.value)
+                    }} value={password} className=" mb-[1.5rem] bg-transparent border-b-[1px] border-b-ma-gray" />
+                    <label className=" text-ma-gray mb-[0.5rem]"> Repeat Password </label>
+                    <input onChange={(e) => {
+                        setRepeatPassword(e.target.value)
+                    }} value={repeatPassword} className=" mb-[1.5rem] bg-transparent border-b-[1px] border-b-ma-gray" />
                     <Button text={"Create an Account"}></Button>
                     <div className=" mt-[1.5rem] flex justify-center">
                         <p className=" mr-[0.5rem]"> Already have an account? </p>
-                        <a href="/" className="text-ma-red"> Login </a>
+                        <a href="/login" className="text-ma-red"> Login </a>
                     </div>
                 </form>
-            </div>   
+            </div>
         </main>
     )
 }
