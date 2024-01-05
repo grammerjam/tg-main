a3dcf84ed73560fedb111a5daded2fd6126efd39
alv-314 <alicov314@gmail.com>
2024-01-05 04:05:50 -0500

    Modified button font weight and size

--- P1/entertainment-app/src/components/Button.jsx
+++ P1/entertainment-app/src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
     // User passes in the desired text, and the onClick function.
     const { text, onClick } = props;
     return (
-        <button onClick={onClick} className="h-[3rem] max-h-[21rem] w-full bg-ma-red text-ma-white hover:bg-ma-white hover:text-black rounded-md ">
+        <button onClick={onClick} className="font-light text-b-med h-[3rem] max-h-[21rem] w-full bg-ma-red text-ma-white hover:bg-ma-white hover:text-black rounded-md ">
             {text}
         </button>
     )
