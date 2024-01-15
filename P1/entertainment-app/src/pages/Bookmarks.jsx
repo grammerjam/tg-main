4e123a10fc78fe39ae59499f9f8d3bfb89b60edf
Jose A <jfacademusic@gmail.com>
2024-01-15 00:30:22 -0500

    changed route element

---
+++ P1/entertainment-app/src/pages/Bookmarks.jsx
@@ -0,0 +1,15 @@
+import MediaContainer from "../components/MediaContainer";
+import Navbar from "../components/Navbar";
+import SearchBar from "../components/SearchBar";
+
+export default function Bookmarks() {
+  return (
+    <div className="flex flex-col desktop:flex-row">
+      <Navbar />
+      <div className="px-[16px] tablet:px-[24px] desktop:pl-[12px] desktop:pr-[36px]">
+        <SearchBar />
+        <MediaContainer pageTitle={"TV Series"} />
+      </div>
+    </div>
+  )
+}
