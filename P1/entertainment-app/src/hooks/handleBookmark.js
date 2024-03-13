a7c99d70374c86fcfd9a661228e5eb652249de12
Jose A <jfacademusic@gmail.com>
2024-03-13 02:36:19 -0400

    deleted console logs from post

--- P1/entertainment-app/src/hooks/handleBookmark.js
+++ P1/entertainment-app/src/hooks/handleBookmark.js
@@ -15,15 +15,12 @@ export const updateBookmark = async (data) => {
             },
             body: JSON.stringify(dataToSend)
         }).then(async (res) => {
-            const response = await res.json()
-            console.log(response)
-
+            await res.json()
         }).then(() => {
-            console.log('hello govnor')
-            return false
+            return true
         })
     } catch (error) {
         console.error('Error:', error);
-        return true
+        return false
     }
 };
