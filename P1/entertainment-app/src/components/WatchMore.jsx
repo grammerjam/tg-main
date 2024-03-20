14ae3cc55fbcbc5c12e15ce1d30f655d45d341ee
Jose A <jfacademusic@gmail.com>
2024-03-20 05:40:54 -0400

    finished watch more structure

--- P1/entertainment-app/src/components/WatchMore.jsx
+++ P1/entertainment-app/src/components/WatchMore.jsx
@@ -1,10 +1,44 @@
+import { useUser } from "@clerk/clerk-react";
+import { useQueryClient } from "@tanstack/react-query"
+import { useCallback, useEffect, useMemo, useState } from "react";
+import TrendingCard from "./TrendingCard";
+
 export default function WatchMore() {
+    const queryClient = useQueryClient()
+    const queryKey = useMemo(() => ["Trending"], []);
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const { user } = useUser();
+    const userEmail = user.primaryEmailAddress.emailAddress;
+    const [watchMoreVideosArray, setWatchMoreVideosArray] = useState([])
+    let queryFn = useCallback(async () => {
+        const trending = fetch(backendUrl + "api/" + `trending/?email=${userEmail}`).then((res) =>
+            res.json(),
+        )
+        return trending
+    }, [backendUrl, userEmail])
+
+    const getWatchMoreData = useCallback(async () => {
+        const watchMoreData = await queryClient.ensureQueryData({ queryKey, queryFn })
+        return watchMoreData
+    }, [queryClient, queryFn, queryKey])
+
+    // const watchMoreData = queryClient.getQueryData(["Trending"])
+    useEffect(() => {
+        getWatchMoreData().then(recommendedVideos => {
+            console.log(recommendedVideos);
+            setWatchMoreVideosArray(recommendedVideos)
+        }).catch(err => {
+            console.error(err);
+        });
+    }, [queryClient, queryFn, queryKey, getWatchMoreData])
+
     return (
         <div className='desktop:flex-grow-[1]'>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
+            {watchMoreVideosArray.map((videoObj) => {
+                return (
+                    <TrendingCard key={videoObj.id} trendingMedia={videoObj} />
+                )
+            })}
         </div>
     )
 }
--- P1/entertainment-app/src/pages/VideoPage.jsx
+++ P1/entertainment-app/src/pages/VideoPage.jsx
@@ -8,25 +8,27 @@ export default function VideoPage() {
 
     return (
         <div className='px-[16px] flex flex-col justify-start items-start gap-[16px] tablet:px-[24px] desktop:pr-[36px] desktop:flex-row w-full desktop:gap-[36px]'>
-            <div className='w-full desktop:w-[75%]'>
+            <div className='w-full desktop:w-[65%] flex flex-col gap-[8px] desktop:gap-[12px]'>
                 <VideoPlayer />
-                <div className='flex flex-col justify-start items-start gap-[4px] tablet:gap-[8px]'>
-                    <h1 className='text-h-sm font-[600] tablet:text-h-med desktop:text-h-lg'>
-                        {id}
-                    </h1>
-                    <ul className='flex gap-[8px] justify-start items-start font-[300]'>
-                        <li>{`2007`}</li>
-                        <li>{"•"}</li>
-                        <li>TV Series</li>
-                        <li>{"•"}</li>
-                        <li>E</li>
-                    </ul>
-                </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quam, ducimus numquam dicta fuga ex inventore molestias quisquam voluptas laborum, iusto sunt recusandae debitis enim. Deserunt reprehenderit ducimus voluptatum dolorum?</p>
-                <div className='w-full flex items-center justify-start gap-[8px]'>
-                    <p className='px-[8px] py-[4px] bg-ma-blue rounded-[10px]'>Nature</p>
-                    <p className='px-[8px] py-[4px] bg-ma-blue rounded-[10px]'>Meditation</p>
-                    <p className='px-[8px] py-[4px] bg-ma-blue rounded-[10px]'>Soothing</p>
+                <div className='flex flex-col w-full gap-[16px]'>
+                    <div className='flex flex-col justify-start items-start gap-[4px] tablet:flex-row tablet:items-baseline tablet:gap-[32px]'>
+                        <h1 className='text-h-sm font-[600] tablet:text-h-med desktop:text-h-lg'>
+                            {id}
+                        </h1>
+                        <ul className='flex gap-[8px] justify-start items-start font-[300]'>
+                            <li>{`2007`}</li>
+                            <li>{"•"}</li>
+                            <li>TV Series</li>
+                            <li>{"•"}</li>
+                            <li>E</li>
+                        </ul>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quam, ducimus numquam dicta fuga ex inventore molestias quisquam voluptas laborum, iusto sunt recusandae debitis enim. Deserunt reprehenderit ducimus voluptatum dolorum?</p>
+                    <div className='w-full flex items-center justify-start gap-[8px]'>
+                        <p className='px-[8px] py-[4px] bg-ma-blue rounded-[10px]'>Nature</p>
+                        <p className='px-[8px] py-[4px] bg-ma-blue rounded-[10px]'>Meditation</p>
+                        <p className='px-[8px] py-[4px] bg-ma-blue rounded-[10px]'>Soothing</p>
+                    </div>
                 </div>
             </div>
             <WatchMore />
