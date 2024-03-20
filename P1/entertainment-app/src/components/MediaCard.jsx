87a8408696da0b0cf1cba77f4334e53ff9618b82
alv-314 <alicov314@gmail.com>
2024-03-19 20:41:40 -0400

    Added aspect ratio to media thumbnails

--- P1/entertainment-app/src/components/MediaCard.jsx
+++ P1/entertainment-app/src/components/MediaCard.jsx
@@ -45,7 +45,7 @@ export default function MediaCard({ media, loading }) {
   }
 
   return (
-    <div className={`mb-[1rem] tablet:mb-[1.5rem] desktop:mb-[2rem] w-[calc((100%-15px)/2)] tablet:w-[calc((100%-60px)/3)] desktop:w-[calc((100%-120px)/4)] text-[11px] tablet:text-b-sm font-[300] text-[#FFFFFFBF] `}>
+    <div className={`mb-[1rem] tablet:mb-[1.5rem] desktop:mb-[2rem] aspect-[3/2] w-[calc((100%-15px)/2)] tablet:w-[calc((100%-60px)/3)] desktop:w-[calc((100%-120px)/4)] text-[11px] tablet:text-b-sm font-[300] text-[#FFFFFFBF] `}>
       <div className={`w-full flex relative justify-end mb-[0.5rem] rounded-lg ${loading && "skeleton"}`}>
         <div className={`cursor-pointer absolute mr-[0.5rem] mt-[0.5rem] tablet:mr-[1rem] tablet:mt-[1rem] w-[2rem] h-[2rem] bg-ma-black rounded-full opacity-50 flex justify-center items-center ${loading && "hidden"} ${isBookmarked ? "bg-ma-white opacity-100" : ""}`}
           onClick={handleBookmarkMedia}
